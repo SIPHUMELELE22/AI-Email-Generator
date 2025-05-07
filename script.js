3410af737987e577aab4018626d69cdef43d8a94
SIPHUMELELE22 <135585328+SIPHUMELELE22@users.noreply.github.com>
2025-05-07 18:15:42 +0200

    Update script.js
    Updated the prompt and context

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ function getResponse(event) {
   event.preventDefault();
   let promptElement = document.querySelector("#prompt");
   let apiKey = "a8410f837a6231f43oc1a1t6c91926bb";
-  let context = "Be polite.";
-  let prompt = `You are an expert email writer. Generate a well-structured email based on this request ${promptElement.value}.`;
+  let context =
+    "You are an expert email writer, generate a well-structured email for the given prompt.";
+  let prompt = `User instructions: Generate an email for ${promptElement.value}.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   if (!promptElement.value) {
